92c60a39bb8e25717ff23b166945783046e640ae
DanielPark <parksungmin3@naver.com>
2023-02-08 00:24:40 +0900

    Vvue

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>90 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>60 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>70 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
